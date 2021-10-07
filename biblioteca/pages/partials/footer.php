f67cf51d9a8762a92a11c3c670c14a997a875f40
jose alexandro <naguibass@hotmail.com>
2021-10-07 17:14:42 -0300

    first commit

---
+++ biblioteca/pages/partials/footer.php
@@ -0,0 +1,98 @@
+      <!-- Footer -->
+      <footer class="bg-dark text-center text-white">
+         <!-- Grid container -->
+         <div class="container p-4">
+            <!-- Section: Social media -->
+            <section class="mb-4">
+               <!-- Facebook -->
+               <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button">
+                  <i class="fab fa-facebook-f"></i>
+               </a>
+               <!-- Twitter -->
+               <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button">
+                  <i class="fab fa-twitter"></i>
+               </a>
+               <!-- Google -->
+               <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button">
+                  <i class="fab fa-google"></i>
+               </a>
+               <!-- Instagram -->
+               <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button">
+                  <i class="fab fa-instagram"></i>
+               </a>
+            </section>
+            <!-- Section: Social media -->
+            <!-- Section: Text -->
+            <section class="mb-4">
+               <p>Biblioteca Sobre escritoras Negras Pernambucanas</p>
+            </section>
+            <!-- Section: Text -->
+            <!-- Section: Links -->
+            <section class="">
+               <!--Grid row-->
+               <div class="row">
+                  <!--Grid column-->
+                  <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+                     <h5 class="text-uppercase">Links</h5>
+                     <ul class="list-unstyled  mb-0">
+                        <li>
+                           <a href="#!" class="text-white text-decoration-none">Inicio</a>
+                        </li>
+                        <li>
+                           <a href="#!" class="text-white text-decoration-none">Quem</a>
+                        </li>
+                        <li>
+                           <a href="#!" class="text-white text-decoration-none">Contato</a>
+                        </li>
+                     </ul>
+                  </div>
+                  <!--Grid column-->
+                  <!--Grid column-->
+                  <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+                     <h5 class="text-uppercase">Links</h5>
+                     <ul class="list-unstyled  mb-0">
+                        <li>
+                           <a href="https://www.ifsertao-pe.edu.br/" class="text-white text-decoration-none">IF Sertão</a>
+                        </li>
+                        <li>
+                           <a href="#!" class="text-white text-decoration-none">Contato</a>
+                        </li>
+                        <li>
+                           <a href="#!" class="text-white text-decoration-none">Contribuir</a>
+                        </li>
+                        <li>
+                           <a href="#!" class="text-white text-decoration-none">Link 4</a>
+                        </li>
+                     </ul>
+                  </div>
+                  <!--Grid column-->
+                  <!--Grid column-->
+                  <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+                  </div>
+                  <!--Grid column-->
+                  <!--Grid column-->
+                  <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+                     <div class="logoIf">
+                        <h4>Instituto Federal</h4>
+                        <img src="/assets/media/logos/iflogo1.png" width="100" height="100">
+                     </div>
+                  </div>
+                  <!--Grid column-->
+               </div>
+               <!--Grid row-->
+            </section>
+            <!-- Section: Links -->
+         </div>
+         <!-- Grid container -->
+         <!-- Copyright -->
+         <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+            © 2020 Copyright: Instituto Federal
+         </div>
+         <!-- Copyright -->
+      </footer>
+      <!-- Footer -->
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.10.1/umd/popper.min.js" integrity="sha512-8jeQKzUKh/0pqnK24AfqZYxlQ8JdQjl9gGONwGwKbJiEaAPkD3eoIjz3IuX4IrP+dnxkchGUeWdXLazLHin+UQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.1.1/js/bootstrap.min.js" integrity="sha512-ewfXo9Gq53e1q1+WDTjaHAGZ8UvCWq0eXONhwDuIoaH8xz2r96uoAYaQCm1oQhnBfRXrvJztNXFsTloJfgbL5Q==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+   </body>
+</html>
